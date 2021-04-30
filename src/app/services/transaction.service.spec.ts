e4d505cbd5753927a93fc15dfd4859de3fc37e3b
Sharad Soni <Sharad.Soni@XEBIAINDIA.LOCAL>
2021-04-30 16:26:26 +0530

    update readme

--- src/app/services/transaction.service.spec.ts
+++ src/app/services/transaction.service.spec.ts
@@ -2,7 +2,6 @@ import { TestBed } from '@angular/core/testing';
 import { TransactionService } from './transaction.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Itransaction } from '../interfaces/itransaction';
-import { Itransactions } from '../interfaces/itransactions';
 
 describe('ApiService', () => {
   let service: TransactionService;
